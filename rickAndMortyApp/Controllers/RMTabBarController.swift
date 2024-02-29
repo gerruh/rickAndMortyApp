f9e6bc9551c5982f2241f4b496bcb1ab419511ec
Herman Volzshsky <avvewyccstrila@gmail.com>
2024-02-29 21:36:53 +0300

    Configutred tabs in TabBar; Configured large titles with UINavigationController

--- rickAndMortyApp/Controllers/RMTabBarController.swift
+++ rickAndMortyApp/Controllers/RMTabBarController.swift
@@ -11,7 +11,43 @@ class RMTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTabs()
     }
+    
+    private func setupTabs() {
+        let settings = RMSettingsViewController()
+        let episodes = RMEpisodesViewController()
+        let characters = RMCharactersViewController()
+        let locations = RMLocationsViewController()
+        
+        let nav1 = UINavigationController(rootViewController: settings)
+        let nav2 = UINavigationController(rootViewController: episodes)
+        let nav3 = UINavigationController(rootViewController: characters)
+        let nav4 = UINavigationController(rootViewController: locations)
+        
+        for nav in [nav1, nav2, nav3, nav4] {
+            nav.navigationItem.largeTitleDisplayMode = .automatic
+            nav.navigationBar.prefersLargeTitles = true
+        }
+        
+        nav1.tabBarItem = UITabBarItem(title: "Episodes",
+                                       image: UIImage(systemName: "play.tv.fill"),
+                                       tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "Characters",
+                                       image: UIImage(systemName: "person.fill"),
+                                       tag: 2)
+        nav3.tabBarItem = UITabBarItem(title: "Locations",
+                                       image: UIImage(systemName: "mappin.square.fill"),
+                                       tag: 3)
+        nav4.tabBarItem = UITabBarItem(title: "Settings",
+                                       image: UIImage(systemName: "gear"),
+                                       tag: 4)
+        
+        setViewControllers([nav1, nav2, nav3, nav4],
+                           animated: true)
+    }
+    
+    
 
 
 }
