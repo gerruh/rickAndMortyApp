929ec5b60b87643649feb6526a0a05dcfd75bf5f
Herman Volzshsky <avvewyccstrila@gmail.com>
2024-02-29 20:59:50 +0300

    Basic project organization according to MVVM pattern

---
+++ rickAndMortyApp/Models/RMCharacter.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacters.swift
+//  rickAndMortyApp
+//
+//  Created by Gera Gerruh on 29.02.24.
+//
+
+import Foundation
---
+++ rickAndMortyApp/Models/RMLocation.swift
@@ -0,0 +1,8 @@
+//
+//  RMLocations.swift
+//  rickAndMortyApp
+//
+//  Created by Gera Gerruh on 29.02.24.
+//
+
+import Foundation
