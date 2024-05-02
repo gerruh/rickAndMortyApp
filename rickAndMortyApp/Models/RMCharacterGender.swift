22bea603f87461126b8008ede83339a32881cf44
Herman Volzshsky <avvewyccstrila@gmail.com>
2024-05-02 16:09:50 +0300

    created the model representations of characters, episodes and locations

---
+++ rickAndMortyApp/Models/RMCharacterGender.swift
@@ -0,0 +1,15 @@
+//
+//  RMCharacterGender.swift
+//  rickAndMortyApp
+//
+//  Created by Gera Gerruh on 2.05.24.
+//
+
+import Foundation
+
+enum RMCharacterGender: String, Codable {
+    case male = "Male"
+    case female = "Female"
+    case genderless = "Genderless"
+    case `unknown` = "Unknown"
+}
---
+++ rickAndMortyApp/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  rickAndMortyApp
+//
+//  Created by Gera Gerruh on 2.05.24.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
